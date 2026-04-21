da897f9db2595ca8057cafe65ee722ac98162444
ayseearslan09-byte <ayseearslan09@gmail.com>
2026-04-21 15:17:27 +0300

    Update weighted_ayse_arslan.py

--- Week02/weighted_ayse_arslan.py
+++ Week02/weighted_ayse_arslan.py
@@ -1,10 +1,8 @@
 import random
-def weighted_srs(data, n, weights=None, with_replacement=False):
-    if with_replacement or weights is None:
-        return random.choices(data, weights=weights, k=n) if with_replacement else random.sample(data, n)
-    res, d_lst, w_lst = [], list(data), list(weights)
-    for _ in range(n):
-        idx = random.choices(range(len(d_lst)), weights=w_lst, k=1)[0]
-        res.append(d_lst.pop(idx))
-        w_lst.pop(idx)
-    return res
+def weighted_srs(data, n, weights, with_replacement=False):
+    if weights is None:
+        return random.choices(data, k=n) if with_replacement else random.sample(data, n)
+    if with_replacement:
+        return random.choices(data, weights=weights, k=n)
+    population = sum([[d]*w for d, w in zip(data, weights)], [])
+    return random.sample(population, n)
